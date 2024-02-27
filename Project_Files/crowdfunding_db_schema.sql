7177cea642dff15c24702b21fac94e0081af21c5
Sara Kleine-Kracht <sarakleine-kracht@Saras-MacBook-Air.local>
2024-02-26 19:19:40 -0600

    updated sql file

--- Project_Files/crowdfunding_db_schema.sql
+++ Project_Files/crowdfunding_db_schema.sql
@@ -1,13 +1,20 @@
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS contacts;
+
 CREATE TABLE category (
-	category_id VARCHAR (50) PRIMARY KEY NOT NULL, 
-	category VARCHAR (50) NOT NULL
+	category_id VARCHAR (50) NOT NULL, 
+	category VARCHAR (50) PRIMARY KEY NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES campaign(category_id)
 );
 SELECT * FROM category
 -----------------------------------------------------
 
 CREATE TABLE subcategory (
-	subcategory_id VARCHAR (50) PRIMARY KEY NOT NULL, 
-	subcategory VARCHAR (50) NOT NULL
+	subcategory_id VARCHAR (50) NOT NULL, 
+	subcategory VARCHAR (50) PRIMARY KEY NOT NULL,
+	FOREIGN KEY (subcategory_id) REFERENCES campaign(subcategory_id)
 );
 SELECT * FROM subcategory
 -----------------------------------------------------
@@ -32,9 +39,10 @@ SELECT * FROM campaign
 -----------------------------------------------------
 
 CREATE TABLE contacts ( 
-	contact_id INT PRIMARY KEY NOT NUL, 
+	contact_id INT PRIMARY KEY NOT NULL, 
 	first_name VARCHAR (50) NOT NULL, 
 	last_name VARCHAR (50) NOT NULL, 
-	email VARCHAR (100) NOT NULL
+	email VARCHAR (100) NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES campaign(contact_id)
 );
 SELECT * FROM contacts
